08320c00094a62f38de6c4d5c1a51fd62ce74ba8
Jhonatan Laguna <hola@jhonatanlaguna.com>
2023-06-01 16:18:48 -0700

    Remove password

--- app/routes/dashboard.User._index.tsx
+++ app/routes/dashboard.User._index.tsx
@@ -5,87 +5,98 @@ import type { User as UserType } from "~/models/dashboard/User.server";
 import { getUserListItems } from "~/models/dashboard/User.server";
 
 export async function loader({ request }: LoaderArgs) {
-  const users = await getUserListItems()
+  const users = await getUserListItems();
 
   return json({ users });
-
-} 
+}
 export default function User() {
   const { users } = useLoaderData<{ users: UserType[] }>();
 
   return (
-  <div className="sm:px-6 lg:px-8">
-    <div className="flex items-center justify-between">
-      <div className="sm:flex-auto">
-        <h1 className="text-xl font-semibold text-gray-900">User</h1>
-        <p className="mt-2 text-sm text-gray-700">
-          A list of all the users.
-        </p>
+    <div className="sm:px-6 lg:px-8">
+      <div className="flex items-center justify-between">
+        <div className="sm:flex-auto">
+          <h1 className="text-xl font-semibold text-gray-900">User</h1>
+          <p className="mt-2 text-sm text-gray-700">A list of all the users.</p>
+        </div>
+        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+          <Link to="/dashboard/User/create">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
+            >
+              Add User
+            </button>
+          </Link>
+        </div>
       </div>
-      <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-        <Link to="/dashboard/User/create">
-          <button type="button"
-            className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
-            Add User
-          </button>
-        </Link>
+      <div className="mt-8 flow-root overflow-hidden">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <table className="w-full text-left">
+            <thead className="bg-white">
+              <tr>
+                <th
+                  scope="col"
+                  className="relative isolate py-3.5 pr-3 text-left text-sm font-semibold text-gray-900"
+                >
+                  ID
+                  <div className="absolute inset-y-0 right-full -z-10 w-screen border-b border-b-gray-200" />
+                  <div className="absolute inset-y-0 left-0 -z-10 w-screen border-b border-b-gray-200" />
+                </th>
+                <th
+                  scope="col"
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+                >
+                  Email
+                </th>
+                <th
+                  scope="col"
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+                >
+                  Name
+                </th>
+                <th
+                  scope="col"
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+                >
+                  Password
+                </th>
+                <th scope="col" className="relative py-3.5 pl-3">
+                  <span className="sr-only">Edit</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user.id}>
+                  <td className="relative py-4 pr-3 text-sm font-medium text-gray-900">
+                    {user.id}
+                    <div className="absolute bottom-0 right-full h-px w-screen bg-gray-100" />
+                    <div className="absolute bottom-0 left-0 h-px w-screen bg-gray-100" />
+                  </td>
+                  <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
+                    {user.email}
+                  </td>
+                  <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
+                    {user.name}
+                  </td>
+                  <td className="px-3 py-4 text-sm text-gray-500">
+                    {user.createdAt}
+                  </td>
+                  <td className="relative py-4 pl-3 text-right text-sm font-medium">
+                    <Link
+                      to={`/dashboard/User/${user.id}`}
+                      className="text-indigo-600 hover:text-indigo-900"
+                    >
+                      Edit
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-    <div className="mt-8 flow-root overflow-hidden">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <table className="w-full text-left">
-          <thead className="bg-white">
-            <tr>
-              <th scope="col" className="relative isolate py-3.5 pr-3 text-left text-sm font-semibold text-gray-900">
-                ID
-                <div className="absolute inset-y-0 right-full -z-10 w-screen border-b border-b-gray-200" />
-                <div className="absolute inset-y-0 left-0 -z-10 w-screen border-b border-b-gray-200" />
-              </th>
-              <th scope="col" className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell">
-                Email
-              </th>
-              <th scope="col" className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell">
-                Name
-              </th>
-              <th scope="col" className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell">
-                Password
-              </th>
-              <th scope="col" className="relative py-3.5 pl-3">
-                <span className="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            { users.map((user) => (
-            <tr key={ user.id }>
-              <td className="relative py-4 pr-3 text-sm font-medium text-gray-900">
-                { user.id }
-                <div className="absolute bottom-0 right-full h-px w-screen bg-gray-100" />
-                <div className="absolute bottom-0 left-0 h-px w-screen bg-gray-100" />
-              </td>
-              <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
-                { user.email }
-              </td>
-              <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
-                { user.name }
-              </td>
-              <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
-                { user.password }
-              </td>
-              <td className="px-3 py-4 text-sm text-gray-500">
-                { user.createdAt}
-              </td>
-              <td className="relative py-4 pl-3 text-right text-sm font-medium">
-                <Link to={`/dashboard/User/${ user.id}`} className="text-indigo-600 hover:text-indigo-900">
-                Edit
-                </Link>
-              </td>
-            </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  </div>
   );
-  }
+}
