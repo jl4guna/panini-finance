180e28d3d7bb8c3ebd2ab752bdafe88a742d64dd
Jhonatan Laguna <hola@jhonatanlaguna.com>
2023-07-30 16:01:41 -0700

    Render time on client

--- app/components/UpcomingEvents.tsx
+++ app/components/UpcomingEvents.tsx
@@ -1,12 +1,21 @@
 import type { Reminder } from "@prisma/client";
-import { format } from "date-fns";
+import { format, formatISO } from "date-fns";
 import React from "react";
 
 type Props = {
   events: Reminder[];
 };
 
+let isHydrating = true;
+
 export default function UpcomingEvents({ events }: Props) {
+  const [isHydrated, setIsHydrated] = React.useState(!isHydrating);
+
+  React.useEffect(() => {
+    isHydrating = false;
+    setIsHydrated(true);
+  }, []);
+
   return (
     <section className="mt-8">
       <h2 className="text-base font-semibold leading-6 text-gray-900">
@@ -24,22 +33,17 @@ export default function UpcomingEvents({ events }: Props) {
             <p className="flex-auto font-semibold text-gray-900 mt-0">
               {event.title}
             </p>
-            <p className="flex-none ml-6">
-              {event.allDay ? (
-                "Todo el día"
-              ) : (
-                <time dateTime={format(new Date(event.date), "yyyy-MM-dd")}>
-                  {format(
-                    new Date(
-                      new Date(event.date).toLocaleString("en-US", {
-                        timeZone: "America/Hermosillo",
-                      }),
-                    ),
-                    "hh:mm a",
-                  )}
-                </time>
-              )}
-            </p>
+            {isHydrated && (
+              <p className="flex-none ml-6">
+                {event.allDay ? (
+                  "Todo el día"
+                ) : (
+                  <time dateTime={formatISO(new Date(event.date))}>
+                    {format(new Date(event.date), "hh:mm a")}
+                  </time>
+                )}
+              </p>
+            )}
           </li>
         ))}
       </ol>
